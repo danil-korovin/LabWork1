9089e1666d4a07b2a2a9d93cbc3ba90d55ce1128
danil-korovin <st128737@student.spbu.ru>
2024-11-22 03:03:16 +0300

    adding main.cpp

---
+++ LabWork1/main.cpp
@@ -0,0 +1,21 @@
+/* Korovin Danil st128737@student.spbu.ru
+   LabWork1
+*/
+
+#include "header.h"
+
+
+int main()
+{   
+    bmpdata header;
+    std::vector<unsigned char>imData;
+    bmpRead("1.bmp", imData, header);
+    clockwise_rotation(imData, header.width, header.height);
+    header.size = sizeof(header) + imData.size();
+    bmpWrite("clockwise_rotation.bmp", imData, header);
+    bmpRead("1.bmp", imData, header); 
+    counterclockwise_rotation(imData, header.width, header.height);
+    header.size = sizeof(header) + imData.size();
+    bmpWrite("counterclockwise_rotation.bmp", imData, header);  
+    return 0;
+}
