6c237b74861b1e795c1e22bd704543768e941390
danil-korovin <st128737@student.spbu.ru>
2024-12-20 16:53:41 +0300

    update header.h

--- LabWork1/header.h
+++ LabWork1/header.h
@@ -1,42 +1,52 @@
 /* Korovin Danil st128737@student.spbu.ru
    LabWork1
 */
-#ifndef HEADER
-#define HEADER
+#ifndef HEADER_H
+#define HEADER_H
 
 #include <iostream>
-#include <vector>
 #include <fstream>
+#include <vector>
 #include <cstdint>
+#include <string>
 
-#pragma pack(push, 1)
-struct bmpdata
+
+struct BMP
 {
-    uint16_t type = 0x4D42; 
-    uint32_t size = 0;  
-    uint16_t res1 = 0;
-    uint16_t res2 = 0;
-    uint32_t offset = 54;
-    uint32_t biSize = 40;
-    int32_t width = 0;
-    int32_t height = 0;
-    uint16_t planes = 1;
-    uint16_t bitCount = 24;
-    uint32_t compres = 0;
-    uint32_t imBytes = 0;
-    int32_t biX = 0;
-    int32_t biY = 0;
-    uint32_t colors = 0;
-    uint32_t imcolors = 0;
+public:
+    void bmpRead(const std::string &namebmp);
+    void bmpWrite(const std::string &namebmp);
+    void Clockwise_rotation();
+    void Counterclockwise_rotation();
+    void GaussFilter();
+    
+private:
+    uint8_t sp[2];
+    struct BMPheader
+    {
+        uint32_t fsize;
+        uint16_t creator1;
+        uint16_t creator2;
+        uint32_t offset;
+    };
+    struct BMPinfo
+    {
+        uint32_t biSize;
+        int32_t width;
+        int32_t height;
+        uint16_t planes;
+        uint16_t bitCount;
+        uint32_t compres;
+        uint32_t imBytes;
+        int32_t biX;
+        int32_t biY;
+        uint32_t colors;
+        uint32_t imcolors;
+    };
+    std::vector<uint8_t> pixdata;
+    BMPheader Header;
+    BMPinfo Info;
 };
 
-
-#pragma pack(pop)  
-
-void bmpRead(const std::string& fileName, std::vector<unsigned char>& imData, bmpdata& header);
-void bmpWrite(const std::string& fileName,  const std::vector<unsigned char>& imData, const bmpdata& header);
-void clockwise_rotation(std::vector<unsigned char>& imData, int& width, int& height);
-void counterclockwise_rotation(std::vector<unsigned char>& imData, int& width, int& height);
-void GaussFilter(std::vector<unsigned char>& imData, int width, int height);
 
 #endif
